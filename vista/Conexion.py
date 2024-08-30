1bf865cc1f6be90e8839d4c35dc26ee80f513cbc
kevvinL <kewinolivaresorlando@gmail.com>
2024-08-30 07:24:15 -0500

    v2

--- vista/Conexion.py
+++ vista/Conexion.py
@@ -6,14 +6,16 @@ class CConexion:
         try:
             conexion = mysql.connector.connect(
                 host="localhost",
+                port = 3306,
                 user="root",
                 password = "",
                 database="Negocio",
             )
-            print("conexion correcta")
+            
+            if conexion.is_connected():
+                print("conexion correcta")
             return conexion
         except mysql.connector.Error as e:
             print("Error en la conexion: {}".format(e))
             return conexion
-
     conexionBaseDeDatos()
